b38ff2be28f9880558a9ff2b01266cad8c0d2bfe
thisisnsh <hadanis.singh@gmail.com>
2025-07-12 14:16:56 -0400

    Initial Commit

---
+++ AIThing/AIThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIThingApp.swift
+//  AIThing
+//
+//  Created by Nishant Singh Hada on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AIThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIThingTests/AIThingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIThingTests.swift
+//  AIThingTests
+//
+//  Created by Nishant Singh Hada on 7/12/25.
+//
+
+import Testing
+@testable import AIThing
+
+struct AIThingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
